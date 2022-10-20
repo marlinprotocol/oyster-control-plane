57dbc236ced9ef5f82525b6e2e460e4015fbfe7d
cabbage <cabbage@example.com>
2022-10-20 07:42:42 +0000

    mvp

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use std::error::Error;
+mod market;
 
-#[tokio::main]
+use std::error::Error;
+use crate::market::*;
+
+#[tokio::main(flavor = "current_thread")]
 pub async fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
+    let js = market::JobsService{};
+    js.run("wss://arb-goerli.g.alchemy.com/v2/KYCa2H4IoaidJPaStdaPuUlICHYhCWo3".to_string()).await;
     Ok(())
 }

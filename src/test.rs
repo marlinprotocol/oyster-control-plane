5c548091a94216d5a878020f1fbd95507ba7b03b
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-08-29 10:58:47 +0530

    use hardcoded rates in tests for consistency and completeness without external rates file

--- src/test.rs
+++ src/test.rs
@@ -10,7 +10,7 @@ use std::str::FromStr;
 use tokio::time::{Duration, Instant};
 use tokio_stream::{Stream, StreamExt};
 
-use crate::market::{GBRateCard, InfraProvider, LogsProvider, RegionalRates};
+use crate::market::{GBRateCard, InfraProvider, LogsProvider, RateCard, RegionalRates};
 
 #[cfg(test)]
 #[derive(Clone, Debug)]
@@ -199,17 +199,17 @@ pub enum Action {
 }
 
 #[cfg(test)]
-pub fn get_rates() -> Option<Vec<RegionalRates>> {
-    let file_path = "./rates.json";
-    let contents = fs::read_to_string(file_path);
-
-    if let Err(err) = contents {
-        println!("Error reading rates file : {err}");
-        return None;
-    }
-    let contents = contents.unwrap();
-    let rates: Vec<RegionalRates> = serde_json::from_str(&contents).unwrap_or_default();
-    Some(rates)
+pub fn get_rates() -> Vec<RegionalRates> {
+    vec![RegionalRates {
+        region: "ap-south-1".to_owned(),
+        rate_cards: vec![RateCard {
+            instance: "c6a.xlarge".to_owned(),
+            min_rate: U256::from_dec_str("29997916666666").unwrap(),
+            cpu: 4,
+            memory: 8,
+            arch: String::from("amd64"),
+        }],
+    }]
 }
 
 #[cfg(test)]

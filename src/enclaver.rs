07f2151c03746e338db659d2f9f26533208156aa
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-10-16 11:10:53 +0530

    add binary to deploy an enclave on an existing instance

---
+++ src/enclaver.rs
@@ -0,0 +1,32 @@
+use cp::aws;
+
+use anyhow::Context;
+use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+/// Control plane for Oyster
+struct Cli {
+    /// AWS profile
+    #[clap(long, value_parser)]
+    profile: String,
+
+    /// Instance id
+    #[clap(long, value_parser)]
+    instance: String,
+
+    /// AWS region
+    #[clap(long, value_parser, default_value = "ap-south-1")]
+    region: String,
+}
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    let aws = aws::Aws::new(cli.profile, String::new(), String::new(), String::new()).await;
+    aws.run_enclave_impl(cli.instance, cli.region, "", 2, 4096, 32);
+
+    Ok(())
+}
